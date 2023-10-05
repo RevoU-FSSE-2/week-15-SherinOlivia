f67af5ca172939b72c817499c159f8ffaabb7a3a
Sherin Olivia <SOChronicle@gmail.com>
2023-10-06 06:45:25 +0700

    chore: fixing client y

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import mongoMiddleware from './middleware/mongoMiddleware';
 import 'dotenv/config'
 import { router, routerX, routerY } from './router'
 import cors from 'cors';
 import escapeHtml from 'escape-html';
+import mongoMiddleware from './middleware/mongoMiddleware';
 import requestIdMiddleware from './middleware/requestMiddleware';
 import helmetConfig from './middleware/helmetMiddleware';
 import morganConfig from './middleware/morganMiddleware';
@@ -21,18 +21,18 @@ app.use(morganConfig)
 
 // cors config:
 const GlobalcorsOptions = {
-    origin: ['https://w15fe.roozone.site', 'https://w15sh-fe.netlify.app', 'http://localhost:5173'],
+    origin: ['https://w15fe.roozone.site', 'https://w15sh-fe.netlify.app'],
 }
 
 const ClientXoptions = {
-    origin: ['https://w15sh-fe.netlify.app', 'http://localhost:5173'],
+    origin: 'https://w15sh-fe.netlify.app',
     methods: ['GET', 'POST'],
 }
 const ClientYoptions = {
-    origin: ['https://w15fe.roozone.site', 'http://localhost:5173'],
+    origin: 'https://w15fe.roozone.site',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }
-  
+
 // router
 app.use(router)
 app.use('/client-x', cors(ClientXoptions), routerX);
