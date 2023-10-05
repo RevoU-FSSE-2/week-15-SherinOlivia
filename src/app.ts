9d3ba04cae077cbb98c71b22ff426428a67fb711
Sherin Olivia <SOChronicle@gmail.com>
2023-10-06 05:34:14 +0700

    chore: testing helmet and morgan middlewares

--- src/app.ts
+++ src/app.ts
@@ -3,33 +3,36 @@ import mongoMiddleware from './middleware/mongoMiddleware';
 import 'dotenv/config'
 import { router, routerX, routerY } from './router'
 import cors from 'cors';
-// import helmet from 'helmet';
-// import escapeHtml from 'escape-html';
-// import morgan from 'morgan';
+import escapeHtml from 'escape-html';
 import requestIdMiddleware from './middleware/requestMiddleware';
+import helmetConfig from './middleware/helmetMiddleware';
+import morganConfig from './middleware/morganMiddleware';
 
 const port = process.env.PORT;
 const app = express()
 app.use(express.urlencoded({ extended: true }));
 
+// middlewares
 app.use(express.json());
 app.use(mongoMiddleware)
 app.use(requestIdMiddleware);
+app.use(helmetConfig)
+app.use(morganConfig)
 
+// cors config:
 const GlobalcorsOptions = {
-    origin: ['https://w15fe.roozone.site', 'https://w15sh-fe.netlify.app', 'http://localhost:5173', 'http://localhost:8000', 'http://localhost:5555'],
+    origin: ['https://w15fe.roozone.site', 'https://w15sh-fe.netlify.app', 'http://localhost:5173'],
 }
 
 const ClientXoptions = {
-    origin: ['https://w15fe.roozone.site', 'https://w15sh-fe.netlify.app', 'http://localhost:5173'],
+    origin: ['https://w15sh-fe.netlify.app', 'http://localhost:5173'],
     methods: ['GET', 'POST'],
 }
 const ClientYoptions = {
-    origin: 'http://localhost:8000',
+    origin: ['https://w15fe.roozone.site', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }
 
-// CORS config
 app.use('/client-x', cors(ClientXoptions));
 app.use('/client-y', cors(ClientYoptions));
 
@@ -37,6 +40,11 @@ app.use('/client-y', cors(ClientYoptions));
 app.use(router)
 app.use('/client-x', routerX);
 app.use('/client-y', routerY);
+
+// xss?
+const htmlScript = '<script>alert("Caed mil");</script>';
+const escapeHtmlUse = escapeHtml(htmlScript);
+console.log(escapeHtmlUse)
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`)
